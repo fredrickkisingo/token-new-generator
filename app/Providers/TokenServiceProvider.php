ea1b3e01ab69ee0ce6ad760f444508b4e3e4f0e8
Fredrick Kisingo <acsdeveloper@africacloudspace.com>
2023-04-05 15:30:45 +0300

    added tokenservice provider to load the migrations

---
+++ app/Providers/TokenServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class TokenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/path/to/migrations');
+    }
+}
+
+?>
